f2fc3c2c23a5b449b39876d4c1242b44e0a42e02
Nikita Guryanov <nikgur177@gmail.com>
2024-11-01 11:58:58 +0300

    methods

--- components/dialogs/device/features/methodDialog.vue
+++ components/dialogs/device/features/methodDialog.vue
@@ -6,7 +6,13 @@ const dialog = defineModel({
   default: false,
 });
 
-const objects = ref([]);
+const objects = ref<any[]>([]);
+
+const selectedObject = ref<any>({});
+
+const selectObject = (object: any) => {
+  selectedObject.value = object;
+};
 
 storeDevice.getDevicesApi({
   type_struct: 'easy',
@@ -35,23 +41,38 @@ storeDevice.getDevicesApi({
 
       <div class="tw-flex">
         <div class="tw-mr-2 tw-w-6/12 tw-rounded tw-border tw-p-3">
-          <div v-if="objects?.methods?.length">
+          <div v-if="objects?.length">
             <div v-for="object in objects" :key="object.id">
-              <p class="tw-text-lg tw-font-semibold">
-                {{ object?.method?.name }}
-              </p>
-              <p>
-                {{ object?.method?.description }}
-              </p>
+              <div class="tw-mb-2 tw-flex tw-items-center tw-justify-between">
+                <p class="tw-max-w-80 tw-truncate tw-text-lg tw-font-semibold">
+                  {{ object.name }}
+                </p>
+                <Button @click="selectObject(object)">
+                  Выбрать
+                </Button>
+              </div>
             </div>
           </div>
           <div v-else>
-            Список Методов пуст
+            Список Обьектов пуст
           </div>
         </div>
         <div class="tw-w-6/12">
           <div class="tw-mb-2 tw-rounded tw-border tw-p-3">
-            Список с возможными методами
+            <!-- Список с возможными методами -->
+            <div v-if="selectedObject.methods?.length">
+              <div v-for="method in selectedObject.methods" :key="method.name">
+                <p class="tw-text-lg tw-font-semibold">
+                  {{ method.name }}
+                </p>
+                <p>
+                  {{ method.description }}
+                </p>
+              </div>
+            </div>
+            <div v-else>
+              Список Методов пуст
+            </div>
           </div>
           <div class="tw-rounded tw-border tw-p-3">
             Выбор параметра метода
