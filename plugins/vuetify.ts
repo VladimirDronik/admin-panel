1146dd2a92c956e9f50e338d7e7cefe8ac4954c0
Nikita Guryanov <nikgur177@gmail.com>
2024-11-21 04:08:19 +0300

    fix build

--- plugins/vuetify.ts
+++
@@ -1,9 +0,0 @@
-// import { createVuetify } from 'vuetify';
-// import '@mdi/font/css/materialdesignicons.css';
-// import { config } from '@/staticData/vuetify';
-// import '@/scss/style.scss';
-
-// export default defineNuxtPlugin((nuxtApp) => {
-//   const vuetify = createVuetify(config);
-//   nuxtApp.vueApp.use(vuetify);
-// });
