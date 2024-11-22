073b8cd4c8ba720b3efe9b4e3297a2d262ce9112
Никита Гурьянов <nikgur177@gmail.com>
2024-11-22 19:38:11 +0300

    create vuepress-project for docs

---
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import { CodeTabs } from "/Users/nikitaguranov/Desktop/admin-panel/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "/Users/nikitaguranov/Desktop/admin-panel/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "/Users/nikitaguranov/Desktop/admin-panel/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
