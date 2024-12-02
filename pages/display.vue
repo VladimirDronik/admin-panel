41803282e9f6134aea63e6481be488363ea9ab34
Nikita Guryanov <nikgur177@gmail.com>
2024-12-02 20:19:37 +0300

    create display page

--- pages/display.vue
+++ pages/display.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
+// Types modules
+import type { APIData } from '~/types/StoreTypes';
+// Helpers modules
+import { roomColor } from '~/helpers/rooms';
 
 const { t } = useI18n();
 
@@ -7,61 +11,82 @@ useHead({
   titleTemplate: computed(() => t('pages.display')),
 });
 
-const page = ref(1);
-const filters = ref([]);
+const apiDisplays = ref<APIData<any>>();
 
-const headers = [
-  {
-    label: 'ID',
-    code: 'id',
-  },
-  {
-    label: 'Тип',
-    code: 'type',
-  },
-  {
-    label: 'Статус',
-    code: 'status',
-  },
-  {
-    label: 'Надпись',
-    code: 'Inscription',
-  },
-  {
-    label: 'Обьект',
-    code: 'object',
-  },
-  {
-    label: 'Помещение',
-    code: 'room',
-  },
-  {
-    label: 'Активно',
-    code: 'active',
-  },
-  {
-    label: 'Действия',
-    code: 'actions',
-  },
-];
+onBeforeMount(async () => {
+  // Get Buttons
+  const dataDeleteDevice: unknown = await useAPI(
+    'http://10.35.16.1:8081/private/cp',
+    {
+      watch: false,
+    },
+  );
+
+  apiDisplays.value = dataDeleteDevice as APIData<any>;
+  //
+});
 
 </script>
 
 <template>
   <SharedUIPanel>
-
     <SharedUIBreadcrumb title="pages.display">
       <Button
         class="text-capitalize"
         icon="pi pi-plus"
-        label="Добавить отображение"
+        label="Добавить помещение"
       />
     </SharedUIBreadcrumb>
-    <BaseTable
-      class="data-table"
-      :headers="headers"
-      :items="[]"
-    />
+    <div class="tw-flex tw-flex-col tw-gap-2">
+      <div v-for="rooms in apiDisplays?.data?.response.room_items" :key="rooms.id" class="tw-rounded-md tw-border tw-p-3 ">
+        <div class="tw-mb-2 tw-flex tw-w-full tw-items-center tw-justify-between">
+          <div class=" tw-flex tw-items-center">
+            <h4 class="tw-mr-2 tw-text-xl" :style="{ color: roomColor(rooms.style) }">
+              {{ rooms.name }}
+            </h4>
+            <div v-for="sensor in rooms.sensors" :key="sensor.id_item" class="tw-mr-2">
+              <img :src="`scenario_items/${sensor.icon}.png`" alt="">
+              {{sensor.current}}
+              <span v-if="sensor.type === 'temperature'">
+                °
+              </span>
+              <span v-else-if="sensor.type === 'humidity'">
+                %
+              </span>
+            </div>
+          </div>
+          <Button
+            label="Добавить кнопку"
+            icon="pi pi-plus"
+            size="small"
+          />
+        </div>
+        <div class="tw-flex tw-flex-wrap">
+          <button type="button" v-for="items in rooms.items" :key="items.id" class="tw-mr-3">
+            <div
+              class="tw tw-relative tw-flex tw-aspect-square tw-w-24 tw-items-center tw-justify-center tw-rounded-md tw-border tw-p-3"
+              :class="{ '!tw-border-inherit': !(items.status === 'on') }"
+              :style="{ borderColor: roomColor(rooms.style) }"
+            >
+              <img :src="`items/${items.icon}.png`" alt="">
+              <Badge
+                v-if="items.group_elements"
+                :style="{ backgroundColor: roomColor(rooms.style) }"
+                :class="{ '!tw-bg-black': !(items.status === 'on') }"
+                class="tw-absolute -tw-right-2 -tw-top-2 tw-rounded-full"
+                rounded
+              >
+                {{ items.group_elements.length }}
+              </Badge>
+            </div>
+            <h5 class="tw-w-24 tw-truncate tw-text-center">
+              {{ items.title }}
+            </h5>
+          </button>
+        </div>
+      </div>
+    </div>
+
   </SharedUIPanel>
 </template>
 
