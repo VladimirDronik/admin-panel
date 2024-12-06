983fb59466a0abb48e237294b7cd2591eb31e659
Stanislava <stasmagergut@gmail.com>
2024-12-06 14:42:17 +0300

    HOME-887: updated DS18B20 form

--- components/forms/byTypes/SensorDS18B20Form.vue
+++ components/forms/byTypes/SensorDS18B20Form.vue
@@ -5,17 +5,19 @@ import { Form } from '@primevue/forms';
 import { zodResolver } from '@primevue/forms/resolvers/zod';
 import { Controller } from '~/types/DevicesEnums';
 import { useControllersViaType } from '~/composables/useControllersViaType';
+import { useControllerPortsViaId } from '~/composables/useControllerPortsViaId';
 
 const { t } = useI18n();
 
 const form = ref({
   controller: null,
-  sdaPort: null,
-  sclPort: null,
+  mode: '',
+  port: '',
+  address: '',
   pollInterval: 300,
   enableGraphingTemp: false,
-  minAvailableTemp: -55,
-  maxAvailableTemp: 125,
+  minAvailableTemp: -40,
+  maxAvailableTemp: 100,
   minAlarmTemp: 0,
   maxAlarmTemp: 0,
 });
@@ -24,11 +26,12 @@ const resolver = ref(
   zodResolver(
     z.object({
       controller: z.number().min(1),
-      sdaPort: z.number().min(1),
-      sclPort: z.number().min(1),
+      mode: z.string().min(1),
+      port: z.number().min(1),
+      address: z.number().min(1),
       pollInterval: z.number().min(300),
-      minAvailableTemp: z.number().min(-55),
-      maxAvailableTemp: z.number().max(125),
+      minAvailableTemp: z.number().min(-40),
+      maxAvailableTemp: z.number().max(100),
     }),
   ),
 );
@@ -38,10 +41,43 @@ getControllersViaType(Controller.MegaD);
 const controllerIdRef = computed(() => form.value.controller);
 const { formattedPorts } = useControllerPortsViaId(controllerIdRef);
 
-const handleSubmit = ({ valid }: { valid: boolean; }) => {
+const address = computed(() => {
+  const { port, mode } = form.value;
+
+  if (!port) {
+    return ''; // Если порт не выбран, возвращаем пустую строку
+  }
+
+  const selectedPort = formattedPorts.value.find((p) => p.value === Number(port));
+
+  if (!selectedPort) {
+    return '';
+  }
+
+  if (mode === '1WBUS') {
+    const sensorAddress = form.value.address.split(';')[1] || '';
+    return `${selectedPort.number};${sensorAddress}`;
+  } if (mode === '1W') {
+    return `${selectedPort.number}`;
+  }
+
+  return '';
+});
+
+watch(
+  address,
+  (newAddress) => {
+    if (form.value.mode !== '1WBUS' || form.value.address === '' || form.value.address.includes(';')) {
+      form.value.address = newAddress;
+    }
+  },
+  { immediate: true },
+);
+
+const handleSubmit = ({ valid }: { valid: boolean;}) => {
   if (valid) {
     console.log('Форма успешно отправлена:', form.value);
-    // Логика отправки данных на сервер
+    console.log('SDA Port ID:', form.value.port);
   } else {
     console.log('Форма содержит ошибки');
   }
@@ -56,20 +92,29 @@ const handleSubmit = ({ valid }: { valid: boolean; }) => {
       <Select :options="controllers" optionLabel="name" optionValue="id" class="tw-w-full" v-model="form.controller" />
     </SharedUILabel>
 
-    <!-- Порт SDA -->
-    <SharedUILabel class="tw-mb-2" :title="t('devices.portSDA')" required :value="form.controller" name="controller">
-      <Select v-model="form.sdaPort" :options="formattedPorts" optionLabel="label" optionValue="value" class="tw-w-full" />
+    <!-- Режим -->
+    <SharedUILabel class="tw-mb-2" :title="t('devices.mode')" required :value="form.mode" name="mode">
+      <div class="tw-flex tw-items-center">
+        <RadioButton class="tw-mr-1" id="single" v-model="form.mode" :value="'1W'" />
+        <label for="single-mode" class="tw-mr-4"> {{ t('devices.single') }}</label>
+        <RadioButton class="tw-mr-1" id="bus" v-model="form.mode" :value="'1WBUS'" />
+        <label for="bus-mode"> {{ t('devices.bus') }}</label>
+      </div>
     </SharedUILabel>
 
-    <!-- Порт SCL -->
-    <SharedUILabel class="tw-mb-2" :title="t('devices.portSCL')" required :value="form.sclPort" name="sclPort">
-      <Select v-model="form.sclPort" :options="formattedPorts" optionLabel="label" optionValue="value" class="tw-w-full" />
+    <!-- Порт -->
+    <SharedUILabel required class="tw-mb-2" :title="t('devices.port')">
+      <Select v-model="form.port" :options="formattedPorts" optionLabel="label" optionValue="value" class="tw-w-full" />
+    </SharedUILabel>
+
+    <!-- Адрес (только для режима 'bus') -->
+    <SharedUILabel required class="tw-mb-2" v-if="form.mode === '1WBUS'" :title="t('devices.address16')">
+      <InputText id="address" v-model="form.address" class="tw-w-full" />
     </SharedUILabel>
 
     <Divider class="tw-mt-0 tw-pb-3" />
 
     <!-- Интервал опроса -->
-
     <SharedUILabel class="tw-mb-2" :title="t('devices.polling')" required :value="form.pollInterval" name="pollInterval">
       <InputNumber suffix=" sec" id="pollInterval" v-model="form.pollInterval" class="tw-mr-10 tw-w-1/4" />
     </SharedUILabel>
