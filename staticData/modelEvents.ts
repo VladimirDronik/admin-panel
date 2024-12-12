476ff97274dcd9c66f3c52e0b0f4fe2467c841b1
Никита Гурьянов <nikgur177@gmail.com>
2024-12-12 04:00:58 +0300

    events data

--- staticData/modelEvents.ts
+++ staticData/modelEvents.ts
@@ -26,6 +26,7 @@ export const itemEventTypes: EventsObject = {
 };
 
 export const deviceEventTypes: EventsObject = {
+  // Controllers
   mega_d: [
     {
       code: 'object.controller.on_load',
@@ -37,6 +38,166 @@ export const deviceEventTypes: EventsObject = {
       code: 'object.controller.on_unavailable',
       name: 'on_unavailable',
       description: 'Контролдлер стал недоступен',
+      actions: [],
+    },
+  ],
+  // Generic Input
+  generic_input: [
+    {
+      code: 'object.generic_input.on_click',
+      name: 'on_click',
+      description: 'Одиночное замыкание',
+      actions: [],
+    },
+    {
+      code: 'object.generic_input.on_double_click',
+      name: 'on_double_click',
+      description: 'Двойное замыкание',
+      actions: [],
+    },
+    {
+      code: 'object.generic_input.on_long_press',
+      name: 'on_long_press',
+      description: 'Длительное замыкание',
+      actions: [],
+    },
+  ],
+  // Sensors
+  bh1750: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  bme280: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  cs: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  ds18b20: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  htu21d: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  outdoor: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  scd4x: [
+    {
+      code: 'object.sensor.on_check',
+      name: 'on_check',
+      description: 'Данные датчика обновлены',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_alarm',
+      name: 'on_alarm',
+      description: 'Данные датчика вышли за пороговые значения',
+      actions: [],
+    },
+  ],
+  motion: [
+    {
+      code: 'object.sensor.on_motion_on',
+      name: 'on_motion_on',
+      description: 'Движение есть',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_motion_off',
+      name: 'on_motion_off',
+      description: 'Движения нет',
+      actions: [],
+    },
+  ],
+  presence: [
+    {
+      code: 'object.sensor.on_motion_on',
+      name: 'on_motion_on',
+      description: 'Движение есть',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_motion_off',
+      name: 'on_motion_off',
+      description: 'Движения нет',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_presence_on',
+      name: 'on_presence_on',
+      description: 'Присутствие есть',
+      actions: [],
+    },
+    {
+      code: 'object.sensor.on_presence_off',
+      name: 'on_presence_off',
+      description: 'Присутствия нет',
       actions: [],
     },
   ],
