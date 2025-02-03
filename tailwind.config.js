14b0e17701fab3afc44ca23e2426e46c63a85dbf
Nikita Guryanov <nikgur177@gmail.com>
2025-02-03 15:00:04 +0300

    change global font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,15 @@ export default {
       display: ['Manrope', 'sans-serif'],
       body: ['Manrope', 'sans-serif'],
     },
+    fontSize: {
+      xs: '0.625rem',
+      sm: '0.75rem',
+      base: '0.875rem',
+      lg: '1rem',
+      xl: '1.125rem',
+      '2xl': '1.375rem',
+      '3xl': '1.75rem',
+    }
   },
   plugins: [tailwindPrimeVue],
 };
